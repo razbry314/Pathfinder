9fa3b239173e501eae9f6f1b73a6c920b92cf541
Rick <rick.zurek@gmail.com>
2016-06-13 16:22:39 -0400

    MVC Site Project
    First commit, set up, main page made, started adding content

---
+++ SimpleCharacterCreator/SimpleCharacterCreator/Controllers/CharCrtController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleCharacterCreator.Controllers
+{
+    public class CharCrtController : Controller
+    {
+        //
+        // GET: /CharCrt/
+
+        public ActionResult AbilityStat()
+        {
+            ViewBag.Message = "Lets make this easy and use a handy metaphor.";
+
+            return View();
+        }
+
+    }
+}
